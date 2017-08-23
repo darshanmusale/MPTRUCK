0482de2bfec56c59f680b351af6d6213a0fafb28
Elvin Baghele <join.eb@gmail.com>
2017-08-23 21:00:03 +0530

    MPTRUCK
    MPTRUCK

--- app/src/main/java/com/ycce/mptruck/OTP.java
+++ app/src/main/java/com/ycce/mptruck/OTP.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -66,7 +67,7 @@ public class OTP extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
-       
+
         // Restore instance state
         if (savedInstanceState != null) {
             onRestoreInstanceState(savedInstanceState);
@@ -76,8 +77,8 @@ public class OTP extends AppCompatActivity implements
         mPhoneNumberViews = (ViewGroup) findViewById(R.id.phone_auth_fields);
         mSignedInViews = (ViewGroup) findViewById(R.id.signed_in_buttons);
 
-        mStatusText = (TextView) findViewById(R.id.status);
-        mDetailText = (TextView) findViewById(R.id.detail);
+       // mStatusText = (TextView) findViewById(R.id.status);
+        //mDetailText = (TextView) findViewById(R.id.detail);
 
         mPhoneNumberField = (EditText) findViewById(R.id.field_phone_number);
         mVerificationField = (EditText) findViewById(R.id.field_verification_code);
@@ -296,25 +297,25 @@ public class OTP extends AppCompatActivity implements
                 // Initialized state, show only the phone number field and start button
                 enableViews(mStartButton, mPhoneNumberField);
                 disableViews(mVerifyButton, mResendButton, mVerificationField);
-                mDetailText.setText(null);
+                //mDetailText.setText(null);
                 break;
             case STATE_CODE_SENT:
                 // Code sent state, show the verification field, the
                 enableViews(mVerifyButton, mResendButton, mPhoneNumberField, mVerificationField);
                 disableViews(mStartButton);
-                mDetailText.setText(R.string.status_code_sent);
+                Toast.makeText(this,R.string.status_code_sent,Toast.LENGTH_LONG);
                 break;
             case STATE_VERIFY_FAILED:
                 // Verification has failed, show all options
                 enableViews(mStartButton, mVerifyButton, mResendButton, mPhoneNumberField,
                         mVerificationField);
-                mDetailText.setText(R.string.status_verification_failed);
+                Toast.makeText(this,R.string.status_verification_failed,Toast.LENGTH_LONG);
                 break;
             case STATE_VERIFY_SUCCESS:
                 // Verification has succeeded, proceed to firebase sign in
                 disableViews(mStartButton, mVerifyButton, mResendButton, mPhoneNumberField,
                         mVerificationField);
-                mDetailText.setText(R.string.status_verification_succeeded);
+               Toast.makeText(this,R.string.status_verification_succeeded,Toast.LENGTH_LONG);
 
                 // Set the verification text based on the credential
                 if (cred != null) {
@@ -328,7 +329,7 @@ public class OTP extends AppCompatActivity implements
                 break;
             case STATE_SIGNIN_FAILED:
                 // No-op, handled by sign-in check
-                mDetailText.setText(R.string.status_sign_in_failed);
+                Toast.makeText(this,R.string.status_sign_in_failed,Toast.LENGTH_LONG);
                 break;
             case STATE_SIGNIN_SUCCESS:
                 // Np-op, handled by sign-in check
@@ -340,7 +341,7 @@ public class OTP extends AppCompatActivity implements
             mPhoneNumberViews.setVisibility(View.VISIBLE);
             mSignedInViews.setVisibility(View.GONE);
 
-            mStatusText.setText(R.string.signed_out);;
+            Toast.makeText(this,R.string.signed_out,Toast.LENGTH_LONG);;
         } else {
             // Signed in
             mPhoneNumberViews.setVisibility(View.GONE);
@@ -350,8 +351,8 @@ public class OTP extends AppCompatActivity implements
             mPhoneNumberField.setText(null);
             mVerificationField.setText(null);
 
-            mStatusText.setText(R.string.signed_in);
-            mDetailText.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+            Toast.makeText(this,R.string.signed_in,Toast.LENGTH_LONG);
+            Toast.makeText(this,getString(R.string.firebase_status_fmt, user.getUid()),Toast.LENGTH_LONG);
         }
     }
 
